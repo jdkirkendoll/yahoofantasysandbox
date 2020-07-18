20b1521f511a5229ef77536be3eda7eb9e8709c1
jdkirkendoll <47092959+jdkirkendoll@users.noreply.github.com>
2020-07-18 01:52:21 -0500

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'APP_KEY': 'udtc3w6q',
-  'APP_SECRET': '6adb06022830ecfbc41a2c3d06410b8e83399704',
+  'APP_KEY': 'dj0yJmk9ZW9PWmFSVGxJRllWJmQ9WVdrOWNETmxaMnhWTjJjbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTU0',
+  'SESSION_SECRET': '961d38c3d371398da486a9dce04f2bcd62df24c0',
   'APP_URL': 'https://takeabigsit.herokuapp.com/'
 };
